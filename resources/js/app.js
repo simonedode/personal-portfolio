0456ec3b35db0f937bd1b4bf58c0cf073a2587d5
Simone Redighieri <simonedode10@gmail.com>
2023-02-17 16:29:46 +0100

    fix(configure): Configured client-side framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
 import './bootstrap';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />)
+    },
+})
