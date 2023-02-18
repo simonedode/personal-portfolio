c2cfc7e564024cf20ebeafdbd391a33db7fbd71d
Simone Redighieri <simonedode10@gmail.com>
2023-02-18 12:41:37 +0100

    fix: Renamed from app.js and import react import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
     <title>Simone Redighieri</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', 'resources/scss/app.scss'])
     @inertiaHead
 </head>
 <body>
