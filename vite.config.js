779ffccb9dbc89cd15f2a10d48bfaf89357b8682
Simone Redighieri <simonedode10@gmail.com>
2023-02-18 12:40:09 +0100

    feat: Added vite plugin for react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react(),
     ],
 });
