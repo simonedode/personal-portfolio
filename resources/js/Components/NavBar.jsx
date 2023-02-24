d9c7af0b7b5dd1e07a849d5309248a005179e8d7
Simone Redighieri <simonedode10@gmail.com>
2023-02-24 17:48:05 +0100

    feat: Edited navbar-component to hide it on scroll

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import '@maicol07/material-web-additions/layout-grid/layout-grid'
 import '@material/web/button/outlined-link-button'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function NavBar(props) {
 
@@ -9,14 +9,26 @@ export default function NavBar(props) {
         {id: 2, label: "ABOUT", href: "#"},
         {id: 3, label: "SKILLS", href: "#card-skills"},
         {id: 4, label: "CONTACT", href: "#"},
-    ])
+    ]);
+    const [show, setShow] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener("scroll", controlNavBar);
+        return () => window.removeEventListener("scroll", controlNavBar);
+    }, [lastScrollY]);
+
+    const controlNavBar = () => {
+        window.scrollY > lastScrollY ? setShow(false) : setShow(true);
+        setLastScrollY(window.scrollY);
+    };
 
     const navButtons = buttons?.map(button =>
         <md-outlined-link-button key={button.id} label={button.label} href={button.href}></md-outlined-link-button>
     );
 
     return (
-        <nav>
+        <nav id="navbar" className={show ? "active" : "hidden"}>
             <md-layout-grid>
                 <div id="nav-profile" grid-span={5}>
                     <img className="profile" src={props.src} alt="Personal image"/>
