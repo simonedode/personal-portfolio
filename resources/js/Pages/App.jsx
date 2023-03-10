706cef3bce74ef4a5c7f56f5134501cf54e16d71
Simone Redighieri <simonedode10@gmail.com>
2023-03-10 15:09:22 +0100

    fix: Edited from a class component to a function component

--- resources/js/Pages/App.jsx
+++ resources/js/Pages/App.jsx
@@ -3,23 +3,17 @@ import NavBar from "../Components/NavBar";
 import CardHome from "../Components/CardHome";
 import Skills from "../Components/Skills";
 
-export default class App extends React.Component {
+export default function App() {
 
-    constructor(props) {
-        super(props);
-    }
+    const name = "Simone Redighieri";
+    const avatar =  "/images/avatar2.png";
 
-    render() {
-        const name = "Simone Redighieri";
-        const avatar =  "/images/avatar2.png";
-
-        return (
-            <>
-                <NavBar name={name} src={avatar}/>
-                <CardHome name={name} info="Full Stack Web Developer | Engineer and Computer Science Student"
-                          src={avatar}/>
-                <Skills />
-            </>
-        )
-    }
+    return (
+        <>
+            <NavBar name={name} src={avatar}/>
+            <CardHome name={name} info="Full Stack Web Developer | Engineer and Computer Science Student"
+                      src={avatar}/>
+            <Skills />
+        </>
+    )
 }
