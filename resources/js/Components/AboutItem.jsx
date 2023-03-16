b42331e28fed7eabb717de9ddd02e4c0ca828821
Simone Redighieri <simonedode10@gmail.com>
2023-03-16 19:37:04 +0100

    fix: Removed comments

--- resources/js/Components/AboutItem.jsx
+++ resources/js/Components/AboutItem.jsx
@@ -5,19 +5,12 @@ import '@maicol07/material-web-additions/card/outlined-card';
 export default function AboutItem(props) {
     return (
         <>
-            {/*<md-layout-grid>
-                <div className="container about-item-title" grid-span={6} grid-align="middle"><h2>{props.title}</h2></div>
-                <div className="container about-item-value" grid-span={6} grid-align="middle">{props.value}</div>*/}
                 <div grid-span={4} >
                     <md-outlined-card class="container about-card" style={{"width": "100%"}}>
                         <h2 className="about-item-title">{props.title}</h2>
                         <p className="about-item-value">{props.value}</p>
                     </md-outlined-card>
                 </div>
-{/*
-                <md-divider grid-span={12}/>
-*/}
-           {/* </md-layout-grid>*/}
         </>
     )
 }
