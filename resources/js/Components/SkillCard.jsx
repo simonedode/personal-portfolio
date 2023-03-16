0deabc6fcbd7302e713f1a6170ff5bf6a80d0df7
Simone Redighieri <simonedode10@gmail.com>
2023-03-16 19:36:10 +0100

    feat: Added images of skills

--- resources/js/Components/SkillCard.jsx
+++ resources/js/Components/SkillCard.jsx
@@ -4,6 +4,9 @@ export default function SkillCard(props) {
     return (
         <div className="skill-card" grid-span-desktop={6} grid-span-tablet={4}>
             <span>{props.name}</span>
+            <span>
+                <img className="skill-image" src={props.src} alt=""/>
+            </span>
             <Line percent={props.percentage} strokeWidth={1} /*strokeColor={"var(--progress-bar-color)"}*/ />
         </div>
     )
--- resources/js/Components/Skills.jsx
+++ resources/js/Components/Skills.jsx
@@ -6,21 +6,21 @@ import {useState} from "react";
 export default function Skills() {
 
     const [skills, setSkills] = useState([
-        {id: 1, name: "HTML5", percentage: 95},
-        {id: 2, name: "CSS3", percentage: 95},
-        {id: 3, name: "JavaScript", percentage: 70},
-        {id: 4, name: "PHP", percentage: 80},
-        {id: 5, name: "Java", percentage: 90},
-        {id: 6, name: "C#", percentage: 80},
-        {id: 7, name: "C", percentage: 80},
-        {id: 8, name: "MySQL", percentage: 90},
-        {id: 9, name: "Laravel", percentage: 90},
-        {id: 10, name: "ReactJS", percentage: 70},
-        {id: 11, name: "Git", percentage: 85},
+        {id: 1, name: "HTML5", percentage: 95, src: "images/skills/html.png"},
+        {id: 2, name: "CSS3", percentage: 95, src: "images/skills/css.png"},
+        {id: 3, name: "JavaScript", percentage: 70, src: "images/skills/javascript.png"},
+        {id: 4, name: "PHP", percentage: 80, src: "images/skills/php.png"},
+        {id: 5, name: "Java", percentage: 90, src: "images/skills/java.png"},
+        {id: 6, name: "C#", percentage: 80, src: "images/skills/cs.png"},
+        {id: 7, name: "C", percentage: 80, src: "images/skills/c.png"},
+        {id: 8, name: "MySQL", percentage: 90, src: "images/skills/mysql.png"},
+        {id: 9, name: "Laravel", percentage: 90, src: "images/skills/laravel.png"},
+        {id: 10, name: "ReactJS", percentage: 70, src: "images/skills/react.png"},
+        {id: 11, name: "Git", percentage: 85, src: "images/skills/git.png"},
     ]);
 
     const skillsCard = skills?.map(skill =>
-        <SkillCard key={skill.id} name={skill.name} percentage={skill.percentage} />
+        <SkillCard key={skill.id} name={skill.name} percentage={skill.percentage} src={skill.src}/>
     );
 
     return (
