ec25a5dfcd4f0bb88e3a109a6107dc18cc2fcf10
Simone Redighieri <simonedode10@gmail.com>
2023-04-04 19:37:29 +0200

    fix(style): Fixed style bugs

--- resources/js/Components/SkillCard.jsx
+++ resources/js/Components/SkillCard.jsx
@@ -3,8 +3,8 @@ import {Line} from "rc-progress";
 export default function SkillCard(props) {
     return (
         <div className="skill-card" grid-span-desktop={6} grid-span-tablet={4}>
-            <span>{props.name}</span>
             <span>
+                {props.name}
                 <img className="skill-image" src={props.src} alt=""/>
             </span>
             <Line percent={props.percentage} strokeWidth={1} strokeColor={"var(--progress-bar-color)"} />
