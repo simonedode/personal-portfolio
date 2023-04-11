6dcf5d255f2efccf8aec8824e25367708a0ff88a
Simone Redighieri <simonedode10@gmail.com>
2023-04-11 16:05:16 +0200

    feat: Added middleware to set locale

---
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
+
+class SetLocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        if (session()->has('locale')){
+            App::setLocale(session()->get('locale'));
+        }
+
+        return $next($request);
+    }
+}
